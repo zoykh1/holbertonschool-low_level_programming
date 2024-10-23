70823dee3ce63d9c6521902ae8d13091cbd145ac
zoykh1 <axaion74@gmail.com>
2024-10-23 13:04:21 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print_alphabet
+ * Return: 0
+*/
+
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
+
+ 
+   
