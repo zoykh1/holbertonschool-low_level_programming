1adef82ab7761d1e15cee35f18b5b7cf62b77b04
zoykh1 <kerimgmc45@gmail.com>
2024-10-25 18:02:02 +0200

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - ajt deux variables
+ * @a: variable a
+ * @b: variable b
+ * Return: la somme
+ */
+
+int add(int a, int b)
+{
+	int n = a + b;
+
+	return (n);
+}
