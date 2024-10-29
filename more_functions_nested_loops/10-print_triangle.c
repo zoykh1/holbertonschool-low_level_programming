3c52b014832d6c830a21724db57ad33b1761ba4c
zoykh1 <kerimgmc45@gmail.com>
2024-10-29 10:23:07 +0100

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,2 +1,34 @@
-void print_triangle(int size);
-// Votre code ici
+#include "main.h"
+
+/**
+ * print_triangle - fais une diagonale
+ *@n: base du triangle
+ * Return: Always le triangle
+ */
+
+void print_triangle(int n)
+
+{
+int espace;
+int hashtag;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (espace = 1; espace <= n; espace++)
+		{
+		for (hashtag = 0; hashtag < n - espace; hashtag++)
+		{
+			_putchar(' ');
+		}
+		for (hashtag = 0; hashtag < espace; hashtag++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+}
