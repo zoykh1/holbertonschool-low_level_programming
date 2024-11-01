37b1044580350cf835dbe0a0387e1857b8196db5
zoykh1 <kerimgmc45@gmail.com>
2024-11-01 15:58:59 +0100

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenates two strings
- *@dest:string to be overwritten (\0) and adding the src
- *@src: string to be append to dest.
- *
- * Return: pointer to the resulting string dest
+ * _strcat - fontction main
+ *@dest: pointeur 1
+ *@src: pointeur 2
+ *Return: 0
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0;
 
-	while (dest[i])
-	{
-		dest++;
+	while (dest[i] != '\0')
+		i++;
 
-	}
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
