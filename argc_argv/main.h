67266727b043a8be6a2bbe1655ff9cfd3a8e6a1a
zoykh1 <kerimgmc45@gmail.com>
2024-11-08 15:26:12 +0100

    main.h

---
+++ argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
