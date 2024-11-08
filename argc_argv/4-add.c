0db63b8329af11dc112d98936949c6b6021aebd4
zoykh1 <kerimgmc45@gmail.com>
2024-11-08 15:30:11 +0100

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,21 +9,21 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j, sum = 0;
+	int i, j, sum = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] < '0' || argv[i][j] > '9')
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        sum += atoi(argv[i]);
-    }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
