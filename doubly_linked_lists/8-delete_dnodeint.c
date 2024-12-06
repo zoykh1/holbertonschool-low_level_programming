23fe2c62b031ad671932b806768772c2319a960f
zoykh1 <kerimgmc45@gmail.com>
2024-12-06 17:53:44 +0100

    trying to fix the requierement bug

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,43 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes node at index of a list
+ * delete_dnodeint_at_index - Deletes node at index of a linked list
  * @head: Pointer to the pointer of the head of the list
  * @index: Index of the node to delete
  * Return: 1 if succeeded, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp = *head;
+	dlistint_t *current = *head;
 	unsigned int i = 0;
 
-	if (!*head)
+	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
 	{
-		*head = temp->next;
-		if (*head)
+		*head = current->next;
+		if (*head != NULL)
 			(*head)->prev = NULL;
-		free(temp);
+		free(current);
 		return (1);
 	}
 
-	while (temp && i < index)
+	while (current != NULL && i < index)
 	{
-		temp = temp->next;
+		current = current->next;
 		i++;
 	}
 
-	if (!temp)
+	if (current == NULL)
 		return (-1);
 
-	temp->prev->next = temp->next;
-	if (temp->next)
-		temp->next->prev = temp->prev;
-	free(temp);
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
 
+	free(current);
 	return (1);
 }
